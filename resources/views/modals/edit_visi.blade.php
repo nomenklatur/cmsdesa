35ef94fd92202b86b54a19cf436974b28512e295
nomenklatur <dimasekaputra2000@gmail.com>
2023-01-28 18:30:32 +0700

    final

--- resources/views/modals/edit_visi.blade.php
+++ resources/views/modals/edit_visi.blade.php
@@ -5,7 +5,7 @@
         <h5 class="modal-title" id="exampleModalLabel">Visi dan Misi</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-    <form action="/admin/pemerintahan/" method="post" id="ubahVisi">
+    <form action="/admin/pemerintahan/" method="post">
       <div class="modal-body">
           @method('PUT')
           @csrf
